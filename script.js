64ec13b9d85cf1a2b20165ee0a58c7254393467d
JoselitoAndre <jandrearaujo@bol.com.br>
2017-03-11 15:58:29 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+var pessoa = {
+	nome: 'Joselito andre'
+	cargo = 'Desenvlvedor'
+
+}
+
+document.getElementByid('nome-cabecalho').innerHTML = pessoa.nome;
+document.getElementByid('cargo-cabecalho').innerHTML = pessoa.cargo;
+document.getElementByid('img-cabecalho').src = pessoa.foto;
